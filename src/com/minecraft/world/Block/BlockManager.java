820102344d092be756f069c52854878e3187e58c
iTitus <mil.vel@web.de>
2014-05-14 14:26:10 +0200

    Update BlockManager.java

--- src/com/minecraft/world/Block/BlockManager.java
+++ src/com/minecraft/world/Block/BlockManager.java
@@ -15,16 +15,22 @@ import java.util.logging.Logger;
  * @author Pascal
  */
 public class BlockManager {
-    private HashMap<Short,Class<?>> registeredBlocks = new HashMap<Short,Class<?>>();
-    public void register(short id,Class<?> block){
-        if(registeredBlocks.containsKey(id))throw new IllegalArgumentException("Block is already registered");
+
+    private HashMap<Short, Class<?>> registeredBlocks = new HashMap<Short, Class<?>>();
+
+    public void register(short id, Class<?> block){
+        if(registeredBlocks.containsKey(id))
+            throw new IllegalArgumentException("Block is already registered");
         registeredBlocks.put(id, block);
     }
+
     public Block createBlock(short id){
         try {
             return (Block)registeredBlocks.get(id).newInstance();
         } catch (Exception ex) {
+
         } 
         return null;
     }
+
 }
